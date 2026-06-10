f5f7274ff019a89fab9a47b05968ea7d4094b3de
Ramon <ramoncfigueiredo@hotmail.com>
2026-06-10 10:42:01 -0300

    Classe Main

---
+++ src/adapter/Main.java
@@ -0,0 +1,16 @@
+package adapter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LegacyLogger legacy = new LegacyLogger();
+
+        Logger logger = new LegacyLoggerAdapter(legacy);
+
+        Application app = new Application(logger);
+
+        app.run();
+
+    }
+}
