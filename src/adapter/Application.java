5982d80cde7348352ca203fd71b39be267824e2b
Ramon <ramoncfigueiredo@hotmail.com>
2026-06-10 10:41:08 -0300

    Cliente

---
+++ src/adapter/Application.java
@@ -0,0 +1,18 @@
+package adapter;
+
+public class Application {
+
+    private Logger logger;
+
+    public Application(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void run() {
+
+        logger.info("Iniciando aplicação");
+
+        logger.error("Falha ao conectar no banco");
+
+    }
+}
