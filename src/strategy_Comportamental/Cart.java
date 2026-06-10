46236b27014deca4491c9bd2d2afd9d51b523d79
Ramon <ramoncfigueiredo@hotmail.com>
2026-06-10 10:56:38 -0300

    Contexto

---
+++ src/strategy_Comportamental/Cart.java
@@ -0,0 +1,21 @@
+package strategy_Comportamental;
+
+public class Cart {
+
+    private double subtotal;
+
+    private DiscountStrategy strategy;
+
+    public Cart(double subtotal, DiscountStrategy strategy) {
+        this.subtotal = subtotal;
+        this.strategy = strategy;
+    }
+
+    public void setDiscountStrategy(DiscountStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public double total() {
+        return strategy.apply(subtotal);
+    }
+}
